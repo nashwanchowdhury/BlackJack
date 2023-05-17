1e5dfd57e19f2b043048f88fca06397575bbdeac
nash <nashwanchowder@gmail.com>
2023-05-17 09:37:05 -0400

    newfiles

---
+++ BlackJack-master/src/com/blackjack/Blackjack.java
@@ -0,0 +1,25 @@
+package com.blackjack;
+
+import java.util.Scanner;
+
+public class Blackjack {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Blackjack!");
+
+        // Create the playing deck
+        Deck playingDeck = new Deck();
+        playingDeck.createFullDeck();
+        playingDeck.shuffleDeck();
+
+        // Create hands for the player and the dealer - hands are created from methods that are made in the deck class
+        Deck playerHand = new Deck();
+        Deck dealerHand = new Deck();
+        
+        // Game loops 
+       
+        
+        
+    }
+}
---
+++ BlackJack-master/src/com/blackjack/Deck.java
@@ -0,0 +1,53 @@
+package com.blackjack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+
+    private ArrayList<Card> deck;
+
+    public Deck() {
+        this.deck = new ArrayList<Card>();
+    }
+
+    public void createFullDeck() {
+        // generate cards
+       
+    }
+
+    public void shuffleDeck(){
+        Collections.shuffle(deck);
+    }
+
+
+
+    public Card getCard(int i){
+        return this.deck.get(i);
+    }
+
+    public void removeCard(int i){
+        this.deck.remove(i);
+    }
+
+    public void addCard(Card addCard) {
+        this.deck.add(addCard);
+    }
+
+    // Get the size of the deck
+    public int deckSize() {
+        return this.deck.size();
+    }
+
+    // Draws from the deck
+    public void draw(Deck comingFrom) {
+       
+    }
+
+    // This will move cards back into the deck to continue playing
+    public void moveAllToDeck(Deck moveTo) {
+      
+    }
+
+
+}
